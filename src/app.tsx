d7930794271787819631df3e98590af2335f8f04
Angel Ponce <acdpf000@gmail.com>
2022-11-26 00:00:13 -0600

    initial commit with initial project structure

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from "react-dom";
+import Index from "./screens";
+
+const render = () => {
+  ReactDOM.render(<Index />, document.body);
+};
+
+export { render };
---
+++ src/screens/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index: React.FC = () => {
+  return <h1>This is the index screen 🔥</h1>;
+};
+
+export default Index;
