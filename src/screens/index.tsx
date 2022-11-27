8e1502d11f150ebf52a67ae820aff9bad299982a
Angel Ponce <acdpf000@gmail.com>
2022-11-26 21:22:01 -0600

    index screen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Typography, Image } from "antd";
+
+const { Title, Text } = Typography;
 
 const Index: React.FC = () => {
-  return <>Content</>;
+  return (
+    <div className="w-full flex flex-col justify-center gap-4">
+      <Title className="text-center font-bold">Bienvenido</Title>
+      <Text className="text-center text-sm">
+        💡 Utiliza <Text keyboard>ctrl</Text>+<Text keyboard>k</Text> para abrir
+        el menú de herramientas disponibles
+      </Text>
+      {/* <Image src="../assets/images/main.gif" width={300} /> */}
+    </div>
+  );
 };
 
 export default Index;
