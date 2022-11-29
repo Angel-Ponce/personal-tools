da6df55ac831bfa7a5d372ee3a4896e276e1d8cc
Angel Ponce <acdpf000@gmail.com>
2022-11-28 23:54:27 -0600

    prepare password manager screen and install jotai as react state manager

---
+++ src/screens/PasswordManager.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "antd";
+import React from "react";
+
+const { Title } = Typography;
+
+const PasswordManager: React.FC = () => {
+  return <Title className="font-bold">Administrador de contraseñas</Title>;
+};
+
+export default PasswordManager;
