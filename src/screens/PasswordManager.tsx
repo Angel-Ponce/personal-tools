01e49cc9eace8b2d2bf34aaff798d1eab42647db
Angel Ponce <acdpf000@gmail.com>
2022-12-08 17:20:01 -0600

    remove w-fit

--- src/screens/PasswordManager.tsx
+++ src/screens/PasswordManager.tsx
@@ -57,7 +57,7 @@ const PasswordManager: React.FC = () => {
 
   return (
     <div className="w-full">
-      <div className="w-fit flex items-center gap-4 mb-4">
+      <div className="flex items-center gap-4 mb-4">
         <LockOutlined className="text-[18px]" />
         <Text className="font-semibold text-lg w-fit text-left">
           Administrador de contraseñas
