154dd3e290bc2931eb7fd930a0ab876d23d75491
Angel Ponce <acdpf000@gmail.com>
2022-12-08 16:19:09 -0600

    reading saved passwords

--- src/screens/PasswordManager.tsx
+++ src/screens/PasswordManager.tsx
@@ -5,7 +5,7 @@ import {
   LockOutlined,
 } from "@ant-design/icons";
 import { Button, Divider, Typography } from "antd";
-import React from "react";
+import React, { useState } from "react";
 
 import savedPasswords from "$assets/passwords/index.json";
 import { DryBadge } from "$atoms";
@@ -13,6 +13,8 @@ import { DryBadge } from "$atoms";
 const { Text } = Typography;
 
 const PasswordManager: React.FC = () => {
+  const [passwords, setPasswords] = useState(savedPasswords.passwords);
+
   return (
     <div className="w-full">
       <div className="w-fit flex items-center gap-4 mb-4">
@@ -25,32 +27,44 @@ const PasswordManager: React.FC = () => {
         <div className="flex justify-end w-full">
           <Button>Nueva</Button>
         </div>
-        <div className="flex items-center justify-between gap-2 p-2">
-          <Text className="flex-1 font-semibold flex gap-2 items-center">
-            <DryBadge className="!px-2 !py-0 font-normal">2</DryBadge>
-            Facebook
-          </Text>
-          <Text className="flex-1">*************</Text>
-          <div className="flex gap-2 items-center">
-            <Button
-              icon={<EyeTwoTone className="text-[12px]" />}
-              size="small"
-            />
-            <Button
-              icon={
-                <EditTwoTone className="text-[12px]" twoToneColor="orange" />
-              }
-              size="small"
-            />
-            <Button
-              icon={
-                <DeleteTwoTone className="text-[12px]" twoToneColor="red" />
-              }
-              size="small"
-            />
-          </div>
+        <div className="flex flex-col">
+          {passwords.map((p, i) => (
+            <div key={i} className="w-full">
+              <div className="flex items-center justify-between gap-2 p-2">
+                <Text className="flex-1 font-semibold flex gap-2 items-center">
+                  <DryBadge className="!px-2 !py-0 font-normal">2</DryBadge>
+                  {p.name}
+                </Text>
+                <Text className="flex-1">{p.password}</Text>
+                <div className="flex gap-2 items-center">
+                  <Button
+                    icon={<EyeTwoTone className="text-[12px]" />}
+                    size="small"
+                  />
+                  <Button
+                    icon={
+                      <EditTwoTone
+                        className="text-[12px]"
+                        twoToneColor="orange"
+                      />
+                    }
+                    size="small"
+                  />
+                  <Button
+                    icon={
+                      <DeleteTwoTone
+                        className="text-[12px]"
+                        twoToneColor="red"
+                      />
+                    }
+                    size="small"
+                  />
+                </div>
+              </div>
+              {i != passwords.length - 1 && <Divider className="!m-0 !p-0" />}
+            </div>
+          ))}
         </div>
-        <Divider className="!m-0 !p-0" />
       </div>
     </div>
   );
